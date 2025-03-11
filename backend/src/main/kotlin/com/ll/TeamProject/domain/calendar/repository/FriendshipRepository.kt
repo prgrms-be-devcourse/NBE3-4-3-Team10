f3c1f9ceacbd34fa728ea43d6e20b035a3014bd2
kingyj00 <onshopyj@gmail.com>
2025-03-11 09:45:05 +0900

    refactor:FriendshipRepository

--- backend/src/main/kotlin/com/ll/TeamProject/domain/calendar/repository/FriendshipRepository.kt
+++ backend/src/main/kotlin/com/ll/TeamProject/domain/calendar/repository/FriendshipRepository.kt
@@ -4,12 +4,15 @@ import com.ll.TeamProject.domain.friend.entity.Friendship
 import com.ll.TeamProject.domain.user.entity.SiteUser
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface FriendshipRepository : JpaRepository<Friendship, Long> {
-    fun findByUser1OrUser2(user1: SiteUser, user2: SiteUser): List<Friendship>
-
-    @Query("SELECT f FROM Friendship f WHERE (f.user1 = :user OR f.user2 = :user)")
+    @Query("SELECT f FROM Friendship f WHERE f.user1 = :user OR f.user2 = :user")
     fun findAllByUser(user: SiteUser): List<Friendship>
 
     fun existsByUser1AndUser2(user1: SiteUser, user2: SiteUser): Boolean
+
+    fun findByUser1AndUser2(user1: SiteUser, user2: SiteUser): Friendship?
+
+    fun findByUser1AndUser2OrUser2AndUser1(user1: SiteUser, user2: SiteUser, user2Alt: SiteUser, user1Alt: SiteUser): Friendship?
 }
